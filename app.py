7dc4528a889fe7c228b472d25b7629b2d9500a2e
Ariba Afzal <areebaansari009@gmail.com>
2026-03-04 06:19:51 +0500

    Update app.py

--- app.py
+++ app.py
@@ -599,5 +599,6 @@ HTML_TEMPLATE = '''
 '''
 
 if __name__ == '__main__':
-    init_db()  # Initialize database on startup
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    init_db()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
